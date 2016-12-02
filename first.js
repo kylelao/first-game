e12ccf903694095bacb667d01f2a48ed33f5886f
kyle lao <kylelao3201@gmail.com>
2016-12-02 08:50:52 -0800

    done

--- first.js
+++ first.js
@@ -1,12 +1,17 @@
 var Pika = document.getElementById("pika");
 var PikaXVal = Number(Pika.getAttribute("x"));
 var PikaYVal = Number(Pika.getAttribute("y"));
+var timeStart = Date.now()
 
+
+var foodEaten = 0
 // randomNumber returns a random number between min and max
 function randomNumber(min,max)
 {
  return Math.floor(Math.random(0,800)*(max-min+1)+min);
 }
+
+//moving character
 document.addEventListener("keydown", function(e) {
 
   if (e.keyCode == 37) {
@@ -29,8 +34,10 @@ else if (e.keyCode == 40) {
 PikaYVal = PikaYVal + 15;
 }
 
-  var foodWidth = 20
-  var foodHeight = 20
+  var foodWidth = 30
+  var foodHeight = 30
+
+  //pokeball1
   var foodX = Number(document.getElementById("pokeball1").getAttribute("x"))
   var foodY = Number(document.getElementById("pokeball1").getAttribute("y"))
   var pokeball1 = document.getElementById("pokeball1");
@@ -38,9 +45,12 @@ PikaYVal = PikaYVal + 15;
   if (PikaXVal > foodX && PikaXVal < foodX + foodWidth && PikaYVal > foodY && PikaYVal < foodY + foodHeight) {
     var randX = randomNumber(50, 750);
     pokeball1.setAttribute("x",randX)
+    foodEaten = foodEaten + 1
+    document.getElementById("scores").textContent = foodEaten
     console.log("overlap");
   }
 
+//pokeball2
   foodX1 = Number(document.getElementById("pokeball2").getAttribute("x"))
   foodY1 = Number(document.getElementById("pokeball2").getAttribute("y"))
   var pokeball2 = document.getElementById("pokeball2");
@@ -50,6 +60,17 @@ PikaYVal = PikaYVal + 15;
   {
     randX = randomNumber(50,750);
     pokeball2.setAttribute("x",randX)
+    foodEaten = foodEaten + 1
+    document.getElementById("scores").textContent = foodEaten
     console.log("overlap");
+  }
+
+  if(foodEaten == 5)
+  {
+    document.getElementById("screen").pauseAnimations()
+    var timeStop = Date.now()
+    var timeDuration = timeStop-timeStart
+    timeDuration = timeDuration/1000
+    document.getElementById("totaltime").textContent = timeDuration
   }
 })
